c892bfbe03e69a5f699d6198b52de4c9af2e2e79
Coagen <farhan.ghaffar@gmail.com>
2026-08-01 09:43:02 +0500

    Streamlit Added

---
+++ streamlit/app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import joblib
+
+# Load the trained model
+model = joblib.load("../model/lr_model.pkl")
+
+# Page title
+st.title("Salary Prediction App")
+
+# Input from user
+years = st.number_input(
+    "Enter Years of Experience",
+    min_value=0.0,
+    step=0.1
+)
+
+# Predict button
+if st.button("Predict Salary"):
+
+    prediction = model.predict([[years]])
+
+    st.success(f"Predicted Salary: ${prediction[0]:,.2f}")
